e6b1c40d6306bb60230af351939e847459285ec9
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-01-27 11:02:33 -0600

    Extra exercises part 1

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -1,12 +1,11 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
+
     @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
     public String add(@PathVariable String num1, @PathVariable String num2){
@@ -31,11 +30,23 @@ public class MathController {
         return "The multiplication is " + response;
     }
 
-    @GetMapping("/divide/{num1}/by/{num2}")
+    @RequestMapping(path = "/divide/{num1}/by/{num2}", method = RequestMethod.GET)
     @ResponseBody
-    public String divide(@PathVariable String num1, @PathVariable String num2){
-        int response = Integer.parseInt(num1) / Integer.parseInt(num2);
+    public String divide(@PathVariable int num1, @PathVariable int num2){
+        try{
+            if(num1 == 0 || num2 == 0){
+                throw new ArithmeticException();
+            }
 
-        return "The division is " + response;
+            int response = num1 / num2;
+
+            return "The division is " + response;
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+            return "Error";
+        }
+
     }
 }
